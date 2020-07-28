f6646e01de24400f02b09fef4b329a0b84cbf61b
Giorgio Garasto <giorgio@garasto.it>
2020-07-28 18:36:16 +0200

    feat: improve app typography

--- src/themes/light.ts
+++ src/themes/light.ts
@@ -6,6 +6,16 @@ const lightTheme = createMuiTheme(
   {
     typography: {
       fontFamily: "'Jost*', sans-serif",
+      h1: {
+        fontSize: '2rem',
+        fontWeight: 800,
+        margin: '1.2rem 0 0.6rem 0',
+      },
+      h2: {
+        fontSize: '1.17rem',
+        fontWeight: 700,
+        margin: '1rem 0 0.5rem 0',
+      },
     },
     palette: {
       primary: {
