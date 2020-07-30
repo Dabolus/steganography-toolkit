8507ee82c5b4d6c977ecc74126b2f4db4e1cb744
Giorgio Garasto <giorgio@garasto.it>
2020-07-30 08:33:36 +0200

    feat: implement abc renderer component

---
+++ src/components/audio/Abc.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent, useEffect, useState } from 'react';
+
+import { Box } from '@material-ui/core';
+
+import abcjs from 'abcjs';
+
+export interface AbcProps {
+  src?: string;
+  params?: any;
+}
+
+const Abc: FunctionComponent<AbcProps> = ({
+  src,
+  params = { responsive: 'resize', viewportHorizontal: true },
+}) => {
+  const [id, setId] = useState<string | null>(null);
+
+  useEffect(() => {
+    setId(`abc-result-${Date.now() + Math.random()}`);
+  }, []);
+
+  useEffect(() => {
+    if (!id || !src) {
+      return;
+    }
+
+    abcjs.renderAbc(id, src, params);
+  }, [id, params, src]);
+
+  return id ? <Box id={id} width={1} /> : null;
+};
+
+export default Abc;
