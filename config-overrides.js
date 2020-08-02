e07cbb6c1e2e465b80759a21a52864dab5594a32
Giorgio Garasto <giorgio@garasto.it>
2020-08-02 19:58:18 +0200

    chore: add support for workers

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,28 @@ const { GenerateSW, InjectManifest } = require('workbox-webpack-plugin');
 const path = require('path');
 
 module.exports = (config) => {
+  const conditionalRulesIndex = config.module.rules.findIndex(
+    ({ oneOf }) => !!oneOf,
+  );
+
+  if (conditionalRulesIndex > -1) {
+    const babelTsLoader = config.module.rules[conditionalRulesIndex].oneOf.find(
+      ({ test, loader }) =>
+        test.toString().includes('ts') && loader.includes('babel-loader'),
+    );
+
+    if (babelTsLoader) {
+      const { loader, options } = babelTsLoader;
+
+      const workerizeLoader = {
+        test: /\.worker\.(js|mjs|ts)$/,
+        use: [require.resolve('workerize-loader'), { loader, options }],
+      };
+
+      config.module.rules[conditionalRulesIndex].oneOf.unshift(workerizeLoader);
+    }
+  }
+
   config.plugins.unshift(
     new CopyPlugin({
       patterns: [
