e75c4edaccc602ed0b28041cce20c36ca24c17d2
Giorgio Garasto <giorgio@garasto.it>
2020-08-03 19:21:40 +0200

    refactor: generate an ast like object for better output customization

--- src/workers/audio/solresol.worker.ts
+++ src/workers/audio/solresol.worker.ts
@@ -1,8 +1,8 @@
 import Fuse from 'fuse.js';
 
-import rawSolresolDictionary from '../../static/solresol/dictionary.json';
+import solresolDictionary from '../../static/solresol/dictionary.json';
 
-const solresolDictionary = rawSolresolDictionary.flatMap(
+const flattenedSolresolDictionary = solresolDictionary.flatMap(
   ({ english = [], ...rest }) =>
     english.map((word: string) => ({ english: word, ...rest })),
 );
@@ -10,20 +10,31 @@ const solresolDictionary = rawSolresolDictionary.flatMap(
 const wordRegex = /([a-z]+)/gi;
 
 const fuse = new Fuse<{ solresol: string; english: string }>(
-  solresolDictionary,
+  flattenedSolresolDictionary,
   {
     keys: ['english'],
     includeScore: true,
   },
 );
 
+export interface SolresolOutputItem {
+  solresol: string;
+  english: string[];
+  preferred?: boolean;
+}
+
+export type SolresolOutput = (string | SolresolOutputItem[])[];
+
 export const computeOutput = async (
   input: string,
-): Promise<{ output: string; hint: string }> => {
-  let output = input;
+): Promise<{
+  output: SolresolOutput;
+  hint: string;
+}> => {
+  const output: SolresolOutput = [];
   let hint = input;
 
-  let outputOffset = 0;
+  let previousIndex = 0;
   let hintOffset = 0;
 
   for (
@@ -33,19 +44,38 @@ export const computeOutput = async (
   ) {
     const [word] = matches;
 
-    const translation = solresolDictionary.find(
-      ({ english }) => english === word.toLowerCase(),
-    )?.solresol;
+    const translations: SolresolOutputItem[] = solresolDictionary.filter(
+      // TODO: remove this optional chaining when dictionary is completed
+      ({ english }) => english?.includes(word.toLowerCase()),
+    );
 
-    if (translation) {
-      output = `${output.slice(
-        0,
-        wordRegex.lastIndex + outputOffset - word.length,
-      )}${translation}${output.slice(wordRegex.lastIndex + outputOffset)}`;
+    if (translations.length > 0) {
+      const sortedTranslations = [...translations].sort(
+        (a, b) =>
+          a.english.indexOf(word.toLowerCase()) -
+          b.english.indexOf(word.toLowerCase()),
+      );
 
-      outputOffset += translation.length - word.length;
+      const [preferredTranslation, ...otherTranslations] = sortedTranslations;
+
+      output.push(
+        input.slice(previousIndex, wordRegex.lastIndex - word.length),
+        [
+          {
+            ...preferredTranslation,
+            preferred: true,
+          },
+          ...otherTranslations,
+        ],
+      );
+
+      previousIndex = wordRegex.lastIndex;
 
       continue;
+    } else {
+      output.push(input.slice(previousIndex, wordRegex.lastIndex));
+
+      previousIndex = wordRegex.lastIndex;
     }
 
     const [
@@ -57,6 +87,7 @@ export const computeOutput = async (
       Math.abs(word.length - possibleWord.length) < 3 &&
       score < 0.01
     ) {
+      // TODO: decide whether to build an AST with the hint too or to keep it simple
       hint = `${hint.slice(
         0,
         wordRegex.lastIndex + hintOffset - word.length,
